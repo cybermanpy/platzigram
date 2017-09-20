04b5362afa3bc8a9b6dbdcb7e0eba13837f0bf65
Diego Saldivar <fhersa@gmail.com>
2017-09-20 15:44:18 -0400

    refactory working area

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,61 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+
+// var presets =  require('babel-preset-es2015');
 
 gulp.task('styles', function() {
-	gulp
-		.src('index.scss')
-		.pipe(sass())
-		.pipe(rename('app.css'))
-		.pipe(gulp.dest('public'));
+    gulp
+        .src('index.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles'])
+gulp.task('assets', function() {
+    gulp
+        .src('assets/*')
+        .pipe(gulp.dest('public'));
+})
+
+function compile (watch){
+    var bundle = watchify(browserify('./src/index.js'));
+
+    function rebundle(){
+        bundle
+            .transform(babel)
+            .bundle()
+            .pipe(source('index.js'))
+            .pipe(rename('app.js'))
+            .pipe(gulp.dest('public'));
+    }
+
+    if (watch) {
+        bundle.on('update', function () {
+            console.log('--> Bundling...');
+            rebundle();
+        })
+    }
+    rebundle();
+}
+
+gulp.task('build', function () {
+    return compile();
+});
+
+gulp.task('watch', function () {
+    return compile(true);
+});
+
+// gulp.task('script', function () {
+//     browserify('./src/index.js')
+//     .transform(babel, {presets: ["es2015"]})
+//     .bundle()
+//     .pipe(source('index.js'))
+// })
+
+gulp.task('default', ['styles', 'assets', 'build'])
